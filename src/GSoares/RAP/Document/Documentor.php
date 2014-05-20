76cd5c430df81bcdc982a309fd67d978e2034bff
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2014-05-19 22:04:36 -0300

    create documentor class

---
+++ src/GSoares/RAP/Document/Documentor.php
@@ -0,0 +1,72 @@
+<?php
+namespace GSoares\RAP\Document;
+
+class Documentor
+{
+    const BOOTSTRAP_CSS_URL = 'cssUrl';
+    const BOOTSTRAP_RESPONSIVE_CSS_URL = 'responsiveCssUrl';
+    const BOOTSTRAP_JAVASCRIPT_URL = 'javascriptUrl';
+    const JQUERY_URL = 'jqueryUrl';
+
+    /**
+     * @var array
+     */
+    private static $classes = [];
+
+    /**
+     * @var array
+     */
+    private static $config = [
+        self::BOOTSTRAP_CSS_URL => 'http://getbootstrap.com/2.3.2/assets/css/bootstrap.css',
+        self::BOOTSTRAP_RESPONSIVE_CSS_URL => 'http://getbootstrap.com/2.3.2/assets/css/bootstrap-responsive.css',
+        self::BOOTSTRAP_JAVASCRIPT_URL => 'http://getbootstrap.com/dist/js/bootstrap.min.js',
+        self::JQUERY_URL => 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'
+    ];
+
+    public static function addClass($class)
+    {
+        self::$classes[] = $class;
+    }
+
+    /**
+     * @return string[]
+     */
+    public static function getClasses()
+    {
+        return self::$classes;
+    }
+
+    /**
+     * @param string $cssUrl
+     * @param string $responsiveCssUrl
+     * @param string $javascriptUrl
+     */
+    public static function configBootstrapResource($cssUrl, $responsiveCssUrl, $javascriptUrl, $jqueryUrl)
+    {
+        self::$config['bootstrap'] = [
+            self::BOOTSTRAP_CSS_URL => $cssUrl,
+            self::BOOTSTRAP_RESPONSIVE_CSS_URL => $responsiveCssUrl,
+            self::BOOTSTRAP_JAVASCRIPT_URL => $javascriptUrl,
+            self::JQUERY_URL => $jqueryUrl
+        ];
+    }
+
+    /**
+     * @param string $configIndex
+     * @return mixed
+     */
+    public static function getConfig($configIndex)
+    {
+        return isset(self::$config[$configIndex]) ? self::$config[$configIndex] : null;
+    }
+
+    /**
+     * @return string
+     */
+    public static function document()
+    {
+        ob_start();
+        include realpath(__DIR__ . '/../../../../template/apidoc.php');
+        return ob_get_clean();
+    }
+}
